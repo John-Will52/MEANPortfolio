1b8ced7ff17eeafd2578737f3036dfe4fea384b3
John-Will52 <johnwill52is@gmail.com>
2020-04-14 21:49:49 -0700

    Routes corrected

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ResumeComponent } from './resume/resume.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WorkshopComponent } from './workshop/workshop.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ResumeComponent,
+    NavBarComponent,
+    AboutMeComponent,
+    WorkshopComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
