0005afd319651bf422dd87d80a440dc3a0b73cf7
John-Will52 <johnwill52is@gmail.com>
2020-04-14 21:32:00 -0700

    Restarted Angular, since I couldn't find the connection problem.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { IndexComponent } from './index/index.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ResumeComponent } from './resume/resume.component';
-import { WorkshopComponent } from './workshop/workshop.component';
 
 
-
-
-const routes: Routes = [
-  {path: '', pathMatch: 'full', component: IndexComponent},
-  {path: '/about', component: AboutMeComponent},
-  {path: '/resume', component: ResumeComponent},
-  {path: '/workshop', component: WorkshopComponent},
-  {path: '**', redirectTo: '/home'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,22 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ResumeComponent } from './resume/resume.component';
-import { WorkshopComponent } from './workshop/workshop.component';
-import { ContactComponent } from './contact/contact.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IndexComponent,
-    AboutMeComponent,
-    ResumeComponent,
-    WorkshopComponent,
-    ContactComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
