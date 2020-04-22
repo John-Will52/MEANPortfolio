bf1c052bbc3d3b7f6191bf9942e28d46351a2112
John-Will52 <johnwill52is@gmail.com>
2020-04-21 17:17:38 -0700

    Just in case

--- client/src/app/about-me/about-me.component.ts
+++ client/src/app/about-me/about-me.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-
+  image: any = ["../../assets/images/MeCollage.jpg", "../../assets/images/BeachCollage.jpg", "../../assets/images/TokyoCollage.jpg", "../../assets/images/EuropeCollage.jpg"]
   constructor() { }
 
   ngOnInit() {
+    this.imageChange();
+  }
+
+  counter(){
+    let i : number = -1;
+    i += 1;
+    if(i == this.image.length){
+      i == 0;
+    }
+
+    
+
+    return i;
+  }
+
+  imageChange(){
+    setInterval(this.counter, 5000);
+    return this.image[this.counter()]
   }
 
 }
